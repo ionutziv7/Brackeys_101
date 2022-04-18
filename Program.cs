cad0ed56a9cdb9a25995f92c828b243d049aff95
Ivanciu Ionut <ionutziv7@protonmail.com>
2022-04-18 21:38:32 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace Brackeys_101
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Change the appearence
+            Console.Title = "Skynet";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WindowHeight = 40;
+
+            //Get a conversation going
+            Console.WriteLine("Hello, what's your name?");
+            Console.ReadLine();
+            Console.WriteLine("My name is RX-9000.\nI'm an AI sent from the future to destroy the mankind.");
+            Console.WriteLine("What is your favorite color?");
+            Console.ReadLine();
+            Console.WriteLine("Cool! Mine is destruction");
             Console.ReadKey();
         }
     }
