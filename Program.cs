365714243f6a5dc44af49297b1fa9b9b189ddf58
Ivanciu Ionut <ionutziv7@protonmail.com>
2022-04-18 20:33:07 +0300

    Created Brackeys101 Series

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Brackeys_101
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+        }
+    }
+}
+
