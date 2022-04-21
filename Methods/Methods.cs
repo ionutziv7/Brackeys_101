cc6f280a461f3954490af403feb5f026771617a7
Ivanciu Ionut <ionutziv7@protonmail.com>
2022-04-21 21:02:38 +0300

    Update Methods.cs

--- Methods/Methods.cs
+++ Methods/Methods.cs
@@ -10,7 +10,6 @@ namespace Brackeys_101
             Random numberGen = new Random();
             string name = "X-" + numberGen.Next(10, 9999);
             int age = numberGen.Next(10, 500);
-
             Console.WriteLine("Hi, I'm " + name);
             Console.WriteLine("I'm " + age + " years old.");
             Console.WriteLine("Oh, I'm an alien.");
@@ -24,13 +23,10 @@ namespace Brackeys_101
         {
             Console.WriteLine("Enter a sentence: ");
             string input = Console.ReadLine();
-
-
             int wordCount = CountWords(input);
             Console.WriteLine("There are " + wordCount + " words in that sentence.");
-
         }
-        static int CountWords(string sentence)
+        public static int CountWords(string sentence)
         {
             int wordCount = sentence.Split(' ').Length;
             return wordCount;
